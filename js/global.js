ab5abea25b1b1b06633e646d22443c6a1d0328fe
Ashish-Kaintur <ashu88793@gmail.com>
2025-01-04 12:45:15 +0530

    first commit

---
+++ js/global.js
@@ -0,0 +1,26 @@
+// Function to toggle dropdown visibility
+function toggleDropdown(dropdownId) {
+    const dropdown = document.getElementById(dropdownId);
+    if (dropdown.style.display === 'block') {
+      dropdown.style.display = 'none';
+    } else {
+      dropdown.style.display = 'block';
+    }
+  }
+  
+// Function to toggle dropdown visibility
+function toggleDropdown(subdropdownId) {
+    const subdropdown = document.getElementById(subdropdownId);
+    if (subdropdown.style.display === 'block') {
+      subdropdown.style.display = 'none';
+    } else {
+      subdropdown.style.display = 'block';
+    }
+  }
+  
+  // <img
+  //         class="w-full bg-center bg-cover"
+  //         src="https://static1.simpleflyingimages.com/wordpress/wp-content/uploads/2021/10/acft-a350-4.jpg"
+  //         alt=""
+
+  
